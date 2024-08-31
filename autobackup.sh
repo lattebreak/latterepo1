4204e6d19856b9b91b1be08fea99d003b100f9bd
lattebreak <darkerwhite08@gmail.com>
2024-09-01 02:55:23 +0800

    Just executed my first cron job, performing backups every 1 minute

---
+++ autobackup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# DECLARE VARIABLES
+SOURCE="/home/datski/testfolder"
+DESTINATION="/home/datski/backupfolder"
+BACKUPNAME="testfolder_bak_$(date +"%I-%M").tar"
+
+# BACKUP "testfolder" to "backupfolder" directory
+cd $DESTINATION
+tar -cf $BACKUPNAME $SOURCE
+
+# crontab command to run every minutes
+# */1 * * * * /home/datski/latterepo1/autobackup.sh
