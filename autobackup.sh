b34cd673f62e647fe2ec9c1c33dedead50cb2381
lattebreak <darkerwhite08@gmail.com>
2024-09-08 02:51:04 +0800

    Just tested the git revert command. It worked! Now fixing the crontab comment again

--- autobackup.sh
+++ autobackup.sh
@@ -9,5 +9,5 @@ BACKUPNAME="testfolder_bak_$(date +"%I-%M").tar"
 cd $DESTINATION
 tar -czf $BACKUPNAME $SOURCE
 
-# crontab command to run every minutes
+# crontab command to run every minute
 # */1 * * * * /home/datski/latterepo1/autobackup.sh
