8955e079482603f905229d65e70f920fc513271c
lattebreak <darkerwhite08@gmail.com>
2024-09-08 02:49:44 +0800

    Revert "corrected grammar on crontab command comment"
    This reverts commit 035acc7205acb46ec34484af65c4b604cd44a2b6.

--- autobackup.sh
+++ autobackup.sh
@@ -9,5 +9,5 @@ BACKUPNAME="testfolder_bak_$(date +"%I-%M").tar"
 cd $DESTINATION
 tar -czf $BACKUPNAME $SOURCE
 
-# crontab command to run every minute
+# crontab command to run every minutes
 # */1 * * * * /home/datski/latterepo1/autobackup.sh
